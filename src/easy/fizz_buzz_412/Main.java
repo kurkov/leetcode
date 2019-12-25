94e8c6d3078d415a9eca5976ed8daa16049c0738
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2019-12-25 22:23:35 +0200

    Add solution of problem 412. Fizz Buzz

---
+++ src/easy/fizz_buzz_412/Main.java
@@ -0,0 +1,12 @@
+package easy.fizz_buzz_412;
+
+/**
+ * @author Aleksey Kurkov. Created on 25.12.2019
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.fizzBuzz(10));
+    }
+}
---
+++ src/easy/fizz_buzz_412/Solution.java
@@ -0,0 +1,31 @@
+package easy.fizz_buzz_412;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Aleksey Kurkov. Created on 25.12.2019
+ * @version 1.0
+ *          Solution of problem 412
+ *          https://leetcode.com/problems/fizz-buzz
+ */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                list.add("Fizz");
+            } else if (i % 3 != 0 && i % 5 == 0) {
+                list.add("Buzz");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else {
+                list.add(Integer.toString(i));
+            }
+        }
+
+        return list;
+    }
+}
