e3bc46046c7d17a00c5054d798aa4a6da6f7aa2f
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2020-01-03 13:12:01 +0200

    Add solution of problem 169. Majority Element

---
+++ src/easy/majority_element_169/Solution.java
@@ -0,0 +1,37 @@
+package easy.majority_element_169;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Aleksey Kurkov. Created on 03.01.20
+ * @version 1.0
+ * Solution of the problem 169
+ * https://leetcode.com/problems/majority-element
+ */
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int minTimesCount = nums.length / 2;
+        int key = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            key = nums[i];
+
+            if (map.containsKey(key)) {
+                int value = map.get(nums[i]);
+
+                value++;
+                map.replace(key, value);
+
+                if (value > minTimesCount) {
+                    break;
+                }
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        return key;
+    }
+}
---
+++ src/easy/majority_element_169/SolutionTest.java
@@ -0,0 +1,35 @@
+package easy.majority_element_169;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Aleksey Kurkov. Created on 03.01.20
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    @Test
+    public void testExample1() {
+        int[] nums = {3, 2, 3};
+        Solution solution = new Solution();
+
+        int expectedResult = 3;
+        int actualResult = solution.majorityElement(nums);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample2() {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        Solution solution = new Solution();
+
+        int expectedResult = 2;
+        int actualResult = solution.majorityElement(nums);
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
