53a5ddac8896c4f5fb1fa6c3c5eb4a0a294ed17e
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2020-01-09 18:01:54 +0200

    Add solution of problem 13. Roman to Integer

---
+++ src/easy/roman_to_integer_13/Solution.java
@@ -0,0 +1,44 @@
+package easy.roman_to_integer_13;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Aleksey Kurkov. Created on 08.01.20
+ * @version 1.0
+ * Solution of problem 13
+ * https://leetcode.com/problems/roman-to-integer
+ */
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (i + 1 < charArray.length) {
+                Character current = charArray[i];
+                Character next = charArray[i + 1];
+
+                if (map.get(current) < map.get(next)) {
+                    result = result - map.get(current);
+                } else {
+                    result = result + map.get(current);
+                }
+            } else {
+                result = result + map.get(charArray[i]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/easy/roman_to_integer_13/SolutionTest.java
@@ -0,0 +1,70 @@
+package easy.roman_to_integer_13;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Aleksey Kurkov. Created on 08.01.20
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testExample1() {
+        String input = "III";
+
+        int expectedResult = 3;
+        int actualResult = solution.romanToInt(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample2() {
+        String input = "IV";
+
+        int expectedResult = 4;
+        int actualResult = solution.romanToInt(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample3() {
+        String input = "IX";
+
+        int expectedResult = 9;
+        int actualResult = solution.romanToInt(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample4() {
+        String input = "LVIII";
+
+        int expectedResult = 58;
+        int actualResult = solution.romanToInt(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample5() {
+        String input = "MCMXCIV";
+
+        int expectedResult = 1994;
+        int actualResult = solution.romanToInt(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
