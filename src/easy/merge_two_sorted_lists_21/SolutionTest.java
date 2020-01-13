74c223c08d88c5e4a0fba1d3588df4da1c3d188b
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2020-01-13 16:39:14 +0200

    Add solution of problem 21. Merge Two Sorted Lists

---
+++ src/easy/merge_two_sorted_lists_21/SolutionTest.java
@@ -0,0 +1,42 @@
+package easy.merge_two_sorted_lists_21;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Aleksey Kurkov. Created on 13.01.20
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testExample1() {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode expectedResult = new ListNode(1);
+        expectedResult.next = new ListNode(1);
+        expectedResult.next.next = new ListNode(2);
+        expectedResult.next.next.next = new ListNode(3);
+        expectedResult.next.next.next.next = new ListNode(4);
+        expectedResult.next.next.next.next.next = new ListNode(4);
+
+        ListNode actualResult = solution.mergeTwoLists(l1, l2);
+
+        assertEquals(expectedResult.toString(), actualResult.toString());
+    }
+}
