dc6762affeec3fe4a9154841e9f96bec5064a4aa
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2020-01-13 11:54:08 +0200

    Add solution of problem 171. Excel Sheet Column Number

---
+++ src/easy/excel_sheet_column_number_171/Solution.java
@@ -0,0 +1,26 @@
+package easy.excel_sheet_column_number_171;
+
+/**
+ * @author Aleksey Kurkov. Created on 13.01.20
+ * @version 1.0
+ * Solution of problem 171
+ * https://leetcode.com/problems/excel-sheet-column-number
+ */
+
+// A  = 26 * 0 + 1 = 1
+// AA = 26 + 1 = 26 * 1 + 1 = 27
+// BA = 26 + 26 + 1 = 26 * 2 + 1 = 53
+// CA = 26 + 26 + 26 + 1 = 26 * 3 + 1 = 79
+
+class Solution {
+    public int titleToNumber(String s) {
+        char[] chars = s.toCharArray();
+        int result = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            result = result * 26 + chars[i] - 64;
+        }
+
+        return result;
+    }
+}
---
+++ src/easy/excel_sheet_column_number_171/SolutionTest.java
@@ -0,0 +1,51 @@
+package easy.excel_sheet_column_number_171;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Aleksey Kurkov. Created on 13.01.20
+ * @version 1.0
+ */
+public class SolutionTest {
+
+    private Solution solution;
+    private String s;
+
+    @Before
+    public void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testExample1() {
+        s = "A";
+
+        int expectedResult = 1;
+        int actualResult = solution.titleToNumber(s);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample2() {
+        s = "AB";
+
+        int expectedResult = 28;
+        int actualResult = solution.titleToNumber(s);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExample3() {
+        s = "ZY";
+
+        int expectedResult = 701;
+        int actualResult = solution.titleToNumber(s);
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
